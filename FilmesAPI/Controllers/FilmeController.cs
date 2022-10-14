0d5cd1ac1793aab67fa4ee1371a6686cb4e4afec
grbalmeida <g.r.almeida@live.com>
2022-10-14 08:28:38 -0300

    Atualizando e removendo dados

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -43,5 +43,43 @@ namespace FilmesAPI.Controllers
 
             return NotFound();
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizaFilme(int id, [FromBody] Filme filmeNovo)
+        {
+            var filme = await _context.Filmes.FirstOrDefaultAsync(filme => filme.Id == id);
+
+            if (filme == null)
+            {
+                return NotFound();
+            }
+
+            filme.Titulo = filmeNovo.Titulo;
+            filme.Diretor = filmeNovo.Diretor;
+            filme.Genero = filmeNovo.Genero;
+            filme.Duracao = filmeNovo.Duracao;
+
+            _context.Filmes.Update(filme);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletaFilme(int id)
+        {
+            var filme = await _context.Filmes.FirstOrDefaultAsync(filme => filme.Id == id);
+
+            if (filme == null)
+            {
+                return NotFound();
+            }
+
+            _context.Filmes.Remove(filme);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
